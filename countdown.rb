6d5344016773fce8dcd25df0adb681d558c0d935
TShipley <taneymail@gmail.com>
2020-01-27 06:23:01 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
+while number = > 0 
+number = 10
+number -= 1
+  puts "{x} second(s)!"
+end
+return "HAPPY NEW YEAR!"
+end
 #write your code here
 
-def countdown
+def countdown_with_sleep
+  
 end
